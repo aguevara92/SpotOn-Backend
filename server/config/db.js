0ea35065a57516d041099c3753c69f2a7358d59b
Andres Guevara <andres.guevara@deliveryhero.com>
2018-03-23 11:33:14 +0100

    Add start script

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 
 export default () => {
     mongoose.Promise = global.Promise;
-    // mongoose.connect('mongodb://localhost/trex');
-    mongoose.connect('localhost', 'trex');
+    mongoose.connect(process.env.MONGODB_URI || 'localhost', 'spotOn');
+    // mongoose.connect('localhost', 'spotOn');
+    // mongoose.connect(process.env.MONGODB_URI);
+
     mongoose.connection
         .once('open', () => console.log('MongoDB running'))
         .on('error', err => console.log(err));
