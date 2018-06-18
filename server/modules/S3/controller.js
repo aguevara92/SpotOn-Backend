cb629c7db186ff0567a957d15ab9b816d5646389
Andres Guevara <andres.guevara@deliveryhero.com>
2018-06-18 16:11:46 +0200

    CPA fixed

--- server/modules/S3/controller.js
+++ server/modules/S3/controller.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 var _ = require('lodash')
 const aws = require('aws-sdk')
+const csv = require('csvtojson')
 
 export const getObject = async (req, res) => {
 	const { adID } = req.params
@@ -19,7 +20,7 @@ export const getObject = async (req, res) => {
 
 	let objectData = []
 
-	s3.getObject(getParams, function(err, data) {
+	s3.getObject(getParams, async function(err, data) {
 		// Handle any error and exit
 		if (err) {
 			console.log(err)
@@ -28,90 +29,108 @@ export const getObject = async (req, res) => {
 
 		// No error happened
 		// Convert Body from a Buffer to a String
-		objectData = csvToObject(data.Body.toString('utf-8')) // Use the encoding necessary
+		const csvString = data.Body.toString('utf-8') // Use the encoding necessary
 		// Convert array of Objects to Object with spot_name as Keys
-		objectData = _.keyBy(objectData, 'spot_name')
 
-		// Get the object from the request
-		let thisAd = objectData[adID]
-		if (!_.isEmpty(thisAd)) {
-			// Fix the formating of the number
-			if (_.isString(thisAd.costs_eur)) {
-				thisAd.costs_eur = thisAd.costs_eur.replace(',', '.')
-				thisAd.costs_eur = thisAd.costs_eur.replace('"', '')
-				thisAd.costs_eur = parseFloat(thisAd.costs_eur)
-			}
-			if (_.isString(thisAd.costs_api_currency)) {
-				thisAd.costs_api_currency = thisAd.costs_api_currency.replace(
-					',',
-					'.'
-				)
-				thisAd.costs_api_currency = thisAd.costs_api_currency.replace(
-					'"',
-					''
-				)
-				thisAd.costs_api_currency = parseFloat(
-					thisAd.costs_api_currency
-				)
-			}
-			if (_.isString(thisAd.acquisitions)) {
-				thisAd.acquisitions = thisAd.acquisitions.replace(',', '.')
-				thisAd.acquisitions = thisAd.acquisitions.replace('"', '')
-				thisAd.acquisitions = parseFloat(thisAd.acquisitions)
-			}
+		csv({
+			noheader: true,
+			output: 'csv'
+		})
+			.fromString(csvString)
+			.then(csvArray => {
+				var jsonObj = []
+				var headers = csvArray[0]
+				for (var i = 1; i < csvArray.length; i++) {
+					var data = csvArray[i]
+					var obj = {}
+					for (var j = 0; j < data.length; j++) {
+						//console.log(data[j])
+						if (data[j] !== undefined || data[j] !== null) {
+							obj[headers[j].trim()] = data[j].trim()
+						}
+					}
+					jsonObj.push(obj)
+				}
+				JSON.stringify(jsonObj)
 
-			let CPA_euro = thisAd.costs_eur / thisAd.acquisitions
-			let CPA_local = thisAd.costs_api_currency / thisAd.acquisitions
+				objectData = _.keyBy(jsonObj, 'spot_name')
 
-			// return the values of CPAs
-			return res.status(200).json({
-				CPA_euro: CPA_euro,
-				CPA_local: CPA_local
+				// Get the object from the request
+				let thisAd = objectData[adID]
+				console.log(thisAd)
+				if (!_.isEmpty(thisAd)) {
+					// Fix the formating of the number
+					if (_.isString(thisAd.costs_eur)) {
+						thisAd.costs_eur = thisAd.costs_eur.replace(',', '.')
+						thisAd.costs_eur = thisAd.costs_eur.replace('"', '')
+						thisAd.costs_eur = parseFloat(thisAd.costs_eur)
+					}
+					if (_.isString(thisAd.costs_lc)) {
+						thisAd.costs_lc = thisAd.costs_lc.replace(',', '.')
+						thisAd.costs_lc = thisAd.costs_lc.replace('"', '')
+						thisAd.costs_lc = parseFloat(thisAd.costs_lc)
+					}
+					if (_.isString(thisAd.acquisitions)) {
+						thisAd.acquisitions = thisAd.acquisitions.replace(
+							',',
+							'.'
+						)
+						thisAd.acquisitions = thisAd.acquisitions.replace(
+							'"',
+							''
+						)
+						thisAd.acquisitions = parseFloat(thisAd.acquisitions)
+					}
+
+					let CPA_euro = thisAd.costs_eur / thisAd.acquisitions
+					let CPA_local = thisAd.costs_lc / thisAd.acquisitions
+
+					// return the values of CPAs
+					return res.status(200).json({
+						CPA_euro: CPA_euro,
+						CPA_local: CPA_local,
+						days_aired: thisAd.datediff,
+						times_aired: thisAd.times_aired,
+						GRP: thisAd.grp
+					})
+				} else {
+					return res.status(400).json({
+						error: true,
+						desc: objectData
+					})
+				}
+
+				//return jsonObj
 			})
-		} else {
-			return res.status(400).json({
-				error: true,
-				desc: objectData
-			})
-		}
 	})
 
 	// Function to convert the CSV to an Array of Objects
 	const csvToObject = csvString => {
-		// The array we're going to build
-		let csvObj = []
-		// Break it into rows to start
-		let csvRows = csvString.split(/\n/)
-		// Take off the first line to get the headers, then split that into an array
-		let csvHeaders = csvRows
-			.shift()
-			.concat(',i')
-			.replace('\r', '')
-			.split(',')
-
-		// Loop through remaining rows
-		for (let rowIndex = 0; rowIndex < csvRows.length; ++rowIndex) {
-			//let rowArray = csvRows[rowIndex].split(',')
-
-			var rowArray = csvRows[rowIndex]
-				.replace(',,', ', ,')
-				.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g)
-
-			// Create a new row object to store our data.
-			let rowObject = (csvObj[rowIndex] = {})
-
-			// Then iterate through the remaining properties and use the headers as keys
-			for (let propIndex = 0; propIndex < rowArray.length; ++propIndex) {
-				// Grab the value from the row array we're looping through...
-				let propValue = rowArray[propIndex].replace('\r', '')
-				// ...also grab the relevant header (the RegExp in both of these removes quotes)
-				let propLabel = csvHeaders[propIndex]
-				if (!isNaN(propValue)) {
-					propValue = parseInt(propValue, 10)
+		//let csvArray = []
+		csv({
+			noheader: true,
+			output: 'csv'
+		})
+			.fromString(csvString)
+			.then(csvArray => {
+				var jsonObj = []
+				var headers = csvArray[0]
+				for (var i = 1; i < csvArray.length; i++) {
+					var data = csvArray[i]
+					var obj = {}
+					for (var j = 0; j < data.length; j++) {
+						//console.log(data[j])
+						if (data[j] !== undefined || data[j] !== null) {
+							obj[headers[j].trim()] = data[j].trim()
+						}
+					}
+					jsonObj.push(obj)
 				}
-				rowObject[propLabel] = propValue
-			}
-		}
-		return csvObj
+				JSON.stringify(jsonObj)
+
+				console.log(jsonObj)
+
+				return jsonObj
+			})
 	}
 }
